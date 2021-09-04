e4a003b18644a3e796875092dac50c399013f68f
dreamlike <B18090512@njupt.edu.cn>
2021-09-04 15:11:03 +0800

    1,add option

--- src/main/java/com/example/starter/Main.java
+++ src/main/java/com/example/starter/Main.java
@@ -4,6 +4,7 @@ package com.example.starter;
 import com.example.starter.Verticle.HttpVerticle;
 import com.example.starter.Verticle.DeviceVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 
 public class Main {
@@ -11,7 +12,9 @@ public class Main {
 
 
   public static void main(String[] args) throws Exception{
-    final Vertx vertx = Vertx.vertx();
+    var v = new VertxOptions()
+      .setInternalBlockingPoolSize(5).setWorkerPoolSize(2);
+    final Vertx vertx = Vertx.vertx(v);
     vertx.deployVerticle(new HttpVerticle());
     vertx.deployVerticle(new DeviceVerticle());
   }
